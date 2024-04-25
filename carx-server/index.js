31c2041c1ea993828f3c0ca09f18c9b2103f5ec4
tahfeezmizan <mizanahmed5316@gmail.com>
2024-04-25 11:43:31 +0600

    stored data on database

--- carx-server/index.js
+++ carx-server/index.js
@@ -25,6 +25,21 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
+
+        const carCollaction = client.db('carxDB').collection('carx');
+
+        
+
+
+        app.post('/cars', async (req, res) => {
+            const cars = req.body;
+            console.log(cars)
+            const newCars = { cars };
+            const result = await carCollaction.insertOne(newCars);
+            res.send(result)
+        })
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
