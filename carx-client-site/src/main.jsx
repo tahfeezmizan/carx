c6f1b2daee6e0fb9bb9a87b1166e7ffcdb0ca50a
tahfeezmizan <mizanahmed5316@gmail.com>
2024-07-12 23:52:34 +0600

    install react agein

--- carx-client-site/src/main.jsx
+++ carx-client-site/src/main.jsx
@@ -1,45 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './components/Home/Home';
-import AddProduct from './components/Product/AddProduct';
-import './index.css';
-import Root from './Root';
-import Login from './components/Header/Login';
-import Register from './components/Header/Register';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root></Root>,
-    children: [
-      {
-        path: "/",
-        element: <Home></Home>,
-        loader: () => fetch('https://carx-server-six.vercel.app/cars')
-      },
-      {
-        path: "/addproduct",
-        element: <AddProduct></AddProduct>,
-      },
-      {
-        path: "/login",
-        element: <Login></Login>
-      },
-      {
-        path: "/register",
-        element: <Register></Register>
-      }
-    ]
-  },
-
-]);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <App />
   </React.StrictMode>,
 )
